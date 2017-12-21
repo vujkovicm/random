864358e89df6ebec9b5e7f37c5a863c99b770bac
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-12-21 13:24:52 -0500

    Create stuff.R

---
+++ stuff.R
@@ -0,0 +1,5 @@
+# don't restrict R rounding of p-value to 2.2E-16 (default: 53 binary digits accuracy)
+.Machine$double.eps # 2.220446e-16
+.Machine$double.xmin # 2.225074e-308
+# soluation
+.Machine$double.eps = .Machine$double.xmin
