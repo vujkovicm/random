092591ad56ccc085a6b9d58606532a5556b4b515
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2017-12-22 13:23:46 -0500

    Update stuff.R

--- stuff.R
+++ stuff.R
@@ -3,3 +3,8 @@
 .Machine$double.xmin # 2.225074e-308
 # soluation
 .Machine$double.eps = .Machine$double.xmin
+
+# systematically rename column names
+# remove everything after _ in column name
+names(df) = gsub(pattern = "_*", replacement = "", x = names(df))
+
