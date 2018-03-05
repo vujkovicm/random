15a37c4791129855b256aae9d6c8465a59009838
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-03-05 13:33:57 -0500

    Update hg19.gene.R

--- hg19.gene.R
+++ hg19.gene.R
@@ -6,8 +6,8 @@
 library("Homo.sapiens")
 locations <- genes(Homo.sapiens, columns = "SYMBOL")
 locations <- as.data.frame(locations)
-locations = subset(locations, is.na(locations$SYMBOL) == F)
-locations$chr = gsub("chr", "", locations$seqnames)
+locations <- subset(locations, is.na(locations$SYMBOL) == F)
+locations$chr <- gsub("chr", "", locations$seqnames)
 locations$skat <- paste0(locations$SYMBOL, " ", locations$chr, ":", locations$start, "-", locations$end)
 
 write.table(locations$skat, "hg19.ucsc.gene.set", row.names = F, col.names = F, quote = F, sep = "")
