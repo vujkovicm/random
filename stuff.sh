2aa68a69932636f9581b30e888fbc3577efcfc61
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-05-18 15:49:07 -0400

    Update stuff.sh

--- stuff.sh
+++ stuff.sh
@@ -1,5 +1,5 @@
 # print duplicate lines
 awk 'seen[$0]++' filename 
 
-#split chrX:123 into two columns
+#split chrX:123 into two columns and convert alleles to uppercase
 awk '{split($1, a, ":"); print substr(a[1], 4), a[2], toupper($2), toupper($3)}' filename 
