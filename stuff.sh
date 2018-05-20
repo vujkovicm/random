7d0bea5da018c12281f689f2222715d5fb4b7831
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-05-20 13:33:54 -0400

    Update stuff.sh

--- stuff.sh
+++ stuff.sh
@@ -3,3 +3,9 @@ awk 'seen[$0]++' filename
 
 #split chrX:123 into two columns and convert alleles to uppercase
 awk '{split($1, a, ":"); print substr(a[1], 4), a[2], toupper($2), toupper($3)}' filename 
+
+# grep exact match from list in file
+grep -w -f lookup.snps filename
+
+# inverse grep on multiple patterns
+grep -v -E 'AGE|SEX|PC' filename
