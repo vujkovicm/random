8d89d84bcc9dc39b6e274caadcbbd38e183d9dee
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2018-05-30 14:33:37 -0400

    Update stuff.sh

--- stuff.sh
+++ stuff.sh
@@ -1,3 +1,20 @@
+# stating the obvious
+bgzip -c file.vcf > file.vcf.gz
+tabix -p vcf file.vcf.gz   
+
+# extract patients from vcf
+bcftools view -Ov --samples-file ids.keeps file.vcf.gz -o new.file.vcf
+
+# single variant association
+rvtest --inVcf file.vcf.gz \
+  --single score,wald \
+  --pheno file.sample \
+  --covar file.sample \
+  --pheno-name phenotype \
+  --covar-name COV1,COV2,COVN \
+  --minMAF 0.0001 \
+  --out file
+
 # print duplicate lines
 awk 'seen[$0]++' filename 
 
