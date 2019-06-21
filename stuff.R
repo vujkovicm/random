f853096b942ebbe6554b0d71024ffb9e2b852926
Marijana Vujkovic <vujkovicm@users.noreply.github.com>
2019-06-21 15:35:21 -0400

    Update stuff.R

--- stuff.R
+++ stuff.R
@@ -92,3 +92,19 @@ df = try(read.table("filename.txt", T), silent = T)
 if (inherits(df, 'try-error')){  
   # what to do if file empty 
 } 
+
+# RSID to CHR:BP
+library("biomaRt")
+snp_mart = useMart(biomart = "ENSEMBL_MART_SNP", 
+                   host    = "grch37.ensembl.org", 
+                   path    = "/biomart/martservice", 
+                   dataset = "hsapiens_snp")
+
+# list of variables (attributes) that can be retrieved
+# listAttributes(mart = snp_mart)
+# list of keywords (filters) that you can merge on 
+# listFilters(mart = snp_mart)
+test <- getBM(attributes = c('refsnp_id', 'chr_name', 'chrom_start', 'allele'), 
+              filters = c('snp_filter'), 
+              values = list(df$MarkerName), 
+              mart = snp_mart)
